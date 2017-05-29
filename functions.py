8400fca3eee97f8a12d98e3cfc93360ccdb966d5
TR McAlexander <tr@mcalexander.me>
2017-05-29 13:16:45 +0000

    Adding functions example

---
+++ functions.py
@@ -0,0 +1,8 @@
+def func1():
+  print('Hello from func1');
+
+def func2(username, job):
+  print('Hello %s, today you will be working on %s' %(username, job))
+
+func1()
+func2('jon', 'changing tabs to space')
